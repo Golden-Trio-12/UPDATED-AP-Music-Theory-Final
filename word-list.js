f33087e66ac4d90d34dbd293a829d56d6b2f5885
Golden-Trio-12 <85909049+Golden-Trio-12@users.noreply.github.com>
2024-05-30 18:00:55 -0700

    Add files via upload

---
+++ word-list.js
@@ -0,0 +1,58 @@
+const wordList = [
+    {
+        word: "ionian",
+        hint: "A mode following the major scale pattern - WWHWWWH."
+    },
+    {
+        word: "dorian",
+        hint: "A minor mode following the pattern WHWWWHW."
+    },
+    {
+        word: "phrygian",
+        hint: "A minor mode following the pattern HWWWHWW."
+    },
+    {
+        word: "lydian",
+        hint: "A major mode following the pattern WWWHWWH."
+    },
+    {
+        word: "mixolydian",
+        hint: "A major mode following the pattern WWHWWHW."
+    },
+    {
+        word: "aolian",
+        hint: "A mode following the minor scale pattern WHWWHWW."
+    },
+    {
+        word: "locrian",
+        hint: "A diminished mode following the pattern HWWHWWW."
+    },
+    {
+        word: "ionian",
+        hint: "A mode with a bright and happy sound."
+    },
+    {
+        word: "dorian",
+        hint: "A mode with a dark yet sweet sound."
+    },
+    {
+        word: "phrygian",
+        hint: "A mode with a dark and exotic sound."
+    },
+    {
+        word: "lydian",
+        hint: "A mode with a bright yet mysterious sound."
+    },
+    {
+        word: "mixolydian",
+        hint: "A mode with a bright sound with a dark edge."
+    },
+    {
+        word: "aolian",
+        hint: "A mode with a dark and sad sound."
+    },
+    {
+        word: "locrian",
+        hint: "A mode with a dissonant and unresolved sound."
+    }
+];
